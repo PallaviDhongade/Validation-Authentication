5e9d80d74db723dfb8b692aae84e22ca280c027b
Pallavi Dhongade <86299156+PallaviDhongade@users.noreply.github.com>
2022-08-11 19:56:35 +0530

    Add files via upload

---
+++ src/test/java/com/springboot/UserDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.in;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
